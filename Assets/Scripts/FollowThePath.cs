0cc45cf1197684964b9e2d9c2eed30f4bb4f208a
Dimaldinho <dlepp@inbox.lv>
2025-04-24 13:23:49 +0300

    Added walking animation to all characters on 'Game' scene

--- Assets/Scripts/FollowThePath.cs
+++ Assets/Scripts/FollowThePath.cs
@@ -1,52 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class FollowThePath : MonoBehaviour
 {
     public Transform[] waypoints;
-
-    [SerializeField]
-    private float moveSpeed = 1f;
-
-    [HideInInspector]
-    public int waypointIndex = 0;
-
+    [SerializeField] private float moveSpeed = 1f;
+    [HideInInspector] public int waypointIndex = 0;
     public bool moveAllowed = false;
 
-    // Use this for initialization
-    
+    private Animator animator;
+
     private void Start()
     {
-        // Place the object at the first waypoint at start
-        transform.position = waypoints[waypointIndex].transform.position;
+        // Snap to the first waypoint
+        transform.position = waypoints[waypointIndex].position;
+
+        // Cache the Animator
+        animator = GetComponent<Animator>();
+        if (animator == null)
+            Debug.LogError($"{name} is missing an Animator component!");
     }
 
-    // Update is called once per frame
     private void Update()
     {
+        // Drive the walk/idle animation
+        animator.SetBool("isWalking", moveAllowed);
+
+        // Only move the transform when flagged
         if (moveAllowed)
-            Move();
+            MoveAlongPath();
     }
 
-    private void Move()
+    private void MoveAlongPath()
     {
-        // Only move if we haven't reached the end of the waypoint array
-        if (waypointIndex <= waypoints.Length - 1)
-        {
-            // Move towards the current target waypoint
-            transform.position = Vector2.MoveTowards(
-                transform.position,
-                waypoints[waypointIndex].transform.position,
-                moveSpeed * Time.deltaTime
-            );
+        if (waypointIndex >= waypoints.Length) return;
 
-            // Once we arrive exactly at the waypoint, advance to the next one
-            if (transform.position == waypoints[waypointIndex].transform.position)
-            {
-                waypointIndex += 1;
-            }
-        }
+        // Move toward the next waypoint
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            waypoints[waypointIndex].position,
+            moveSpeed * Time.deltaTime
+        );
+
+        // Once we arrive, advance the index
+        if ((Vector2)transform.position == (Vector2)waypoints[waypointIndex].position)
+            waypointIndex++;
     }
 }
